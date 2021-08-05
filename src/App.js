cb75fa0838ba4faa6ce9efc96a74c7412a579296
jorgetl-25 <jorgetl25@hotmail.com>
2021-08-05 18:28:22 -0500

    primer commit al serv

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import CharacterTable from './components/CharacterTable'
+import axios from 'axios'
+import React , {useEffect,useState} from 'react'
+import Search from './components/Search'
+
+// Public Key e6bd17e3b3c12d988d988b55d2af564a
+
+// PRIVATE KEY 6644ca9eb74b793765aaf407150c08df08fb84d3
+
+// Hash= b4ab559bb3302001b5a34d0d7a89393f
+
+//primero ponemo la public key luego el hash
+
+const hash =  "b4ab559bb3302001b5a34d0d7a89393f";
 
 function App() {
+  const[items,setItems] = useState([])
+  const[isLoading,setLoading] = useState(true)
+  const [query,setQuery] = useState('')
+
+  useEffect(()=>{
+      const fetch = async()=>{
+        if(query===''){
+          // checking if favorites array is empty or does not exist
+          if(localStorage.getItem('favorites')==='[]' || !localStorage.getItem('favorites')){
+            localStorage.setItem('favorites', '[]')
+            const result = await axios(`http://gateway.marvel.com/v1/public/characters?apikey=e6bd17e3b3c12d988d988b55d2af564a&hash=${hash}`)
+            console.log(result.data.data.results)
+            setItems(result.data.data.results)
+            setLoading(false) 
+          }else{
+            let favorite = JSON.parse(localStorage.getItem('favorites'))
+            setItems(favorite)
+            setLoading(false)
+          }
+          
+          
+        }else{
+          const result = await axios(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&apikey=e6bd17e3b3c12d988d988b55d2af564a&hash=${hash}`)
+          console.log(result.data.data.results)
+          setItems(result.data.data.results)
+          setLoading(false)
+        }
+      
+    }
+
+    fetch()
+  },[query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Search search={(q)=>setQuery(q)}></Search>
+      <CharacterTable items={items} isLoading={isLoading} />
     </div>
   );
 }
